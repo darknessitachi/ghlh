8fb7de716afe7822773e5f043e0eac4a4dcd5561
Robin Liu <lqj.liu@gmail.com>
2013-04-09 21:08:11 +0800

    Fix compile issue

--- src/test/com/ghlh/stockquotes/InternetStockQuotesInquirerTest.java
+++ src/test/com/ghlh/stockquotes/InternetStockQuotesInquirerTest.java
@@ -10,13 +10,12 @@ public class InternetStockQuotesInquirerTest {
 	@Test
 	public void testGetStockQuotesBean() {
 		try {
-			String[][] stocks = {{"600036", "招商银行"}, {"000002", "万科A"},
-					{"300077", "国民技术"}};
+			String[][] stocks = { { "600036", "招商银行" }, { "000002", "万科A" },
+					{ "300077", "国民技术" } };
 
 			for (int i = 0; i < stocks.length; i++) {
-				InternetStockQuotesInquirer internetStockQuotesInquirer = new InternetStockQuotesInquirer();
-				StockQuotesBean stockQuotesBean = internetStockQuotesInquirer
-						.getStockQuotesBean(stocks[i][0]);
+				StockQuotesBean stockQuotesBean = InternetStockQuotesInquirer
+						.queryStock(stocks[i][0]);
 				assert (stockQuotesBean.getStockId().equals(stocks[i][0]));
 				assert (stockQuotesBean.getName().equals(stocks[i][1]));
 				assert (stockQuotesBean.getCurrentPrice() >= 0);

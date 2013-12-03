4bce329635215849dae227e3fe337a8eebe38651
lqjliu <lqj.liu@gmail.com>
2013-12-03 14:09:06 +0800

    refactor

--- src/java/com/ghlh/autotrade/OneTimeJobUtil.java
+++ src/java/com/ghlh/autotrade/OneTimeJobUtil.java
@@ -6,10 +6,14 @@ import com.ghlh.data.db.MonitorstockDAO;
 import com.ghlh.data.db.MonitorstockVO;
 import com.ghlh.strategy.OneTimeStrategy;
 import com.ghlh.util.ReflectUtil;
+import com.ghlh.util.StockMarketUtil;
 
 public class OneTimeJobUtil {
 
 	public static void processOneTimeStrategy(String oneTimeType) {
+		if (StockMarketUtil.isMarketRest()) {
+			return;
+		}
 		List monitorStocksList = MonitorstockDAO.getMonitorStock();
 		for (int i = 0; i < monitorStocksList.size(); i++) {
 			MonitorstockVO monitorstockVO = (MonitorstockVO) monitorStocksList

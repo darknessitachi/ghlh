e3d376143c61c4e704422b813706369cefac97b0
lqjliu <lqj.liu@gmail.com>
2014-03-02 22:50:37 +0800

    Stat

---
+++ src/main/java/com/ghlh/strategy/morning4percent/Morning4Stat.java
@@ -0,0 +1,102 @@
+package com.ghlh.strategy.morning4percent;
+
+import java.util.Date;
+import java.util.List;
+
+import com.ghlh.data.db.GhlhDAO;
+import com.ghlh.data.db.StockdailyinfoVO;
+import com.ghlh.stockquotes.InternetStockQuotesInquirer;
+import com.ghlh.stockquotes.StockQuotesBean;
+import com.ghlh.util.DateUtil;
+import com.ghlh.util.MathUtil;
+
+public class Morning4Stat {
+
+	public static void main(String[] args) {
+		Date date = DateUtil.getDate(2014, 1, 7);
+		double lowPercentage = 3.5;
+		double highPercentage = 5.5;
+		double lostPercentage = 0.08;
+		double winPercentage = 0.12;
+		int stockCount = 1;
+
+		Date now = new Date();
+
+		while (date.before(now)) {
+			String sDate = DateUtil.formatDay(date);
+			String sql = "SELECT * FROM stockdailyinfo10 WHERE DATE LIKE '"
+					+ sDate + "%' AND zdf >= " + lowPercentage + " AND zdf <= "
+					+ highPercentage + " ORDER BY hsl DESC";
+
+			List list = GhlhDAO.list(sql, "com.ghlh.data.db.StockdailyinfoVO",
+					0, stockCount + 2);
+			int no = 0;
+
+			for (int i = 0; i < list.size(); i++) {
+				StockdailyinfoVO stockdailyinfoVO = (StockdailyinfoVO) list
+						.get(i);
+				StockQuotesBean sqb = InternetStockQuotesInquirer.getInstance()
+						.getStockQuotesBean(stockdailyinfoVO.getStockid());
+				if (sqb.getName().indexOf("ST") >= 0) {
+					continue;
+				}
+				no++;
+				processBuyStockResult(stockdailyinfoVO, sqb, date,
+						winPercentage, lostPercentage);
+				if (no >= stockCount) {
+					break;
+				}
+			}
+			date = DateUtil.getNextDay(date);
+
+		}
+
+	}
+
+	public static void processBuyStockResult(StockdailyinfoVO stockdailyinfoVO,
+			StockQuotesBean sqb, Date date, double winPercentage,
+			double lostPercentage) {
+		double buyPrice = stockdailyinfoVO.getCurrentprice();
+		double winPrice = MathUtil.formatDoubleWith2QuanShe(buyPrice
+				* (1 + winPercentage));
+		double lostPrice = MathUtil.formatDoubleWith2QuanJin(buyPrice
+				* (1 - lostPercentage));
+
+		Date next = DateUtil.getNextDay(date);
+		String sNext = DateUtil.formatDay(next);
+		String sql1 = "SELECT * FROM stockdailyinfo WHERE stockid = '"
+				+ stockdailyinfoVO.getStockid() + "' AND DATE > '" + sNext
+				+ "' ORDER BY Date";
+
+		List dailyInfoList = GhlhDAO.list(sql1,
+				"com.ghlh.data.db.StockdailyinfoVO");
+		for (int j = 0; j < dailyInfoList.size(); j++) {
+			StockdailyinfoVO stockdailyinfoVO1 = (StockdailyinfoVO) dailyInfoList
+					.get(j);
+			if (stockdailyinfoVO1.getCurrentprice() == 0) {
+				continue;
+			}
+			double highPrice = stockdailyinfoVO1.getHighestprice();
+			double lowPrice = stockdailyinfoVO1.getLowestprice();
+			if (highPrice >= winPrice && lowPrice <= lostPrice) {
+				System.out.println("同时在一天达到盈利 和 亏损 价格");
+				break;
+			}
+			if (highPrice >= winPrice) {
+				System.out.println(date + " " + sqb.getStockId() + " "
+						+ sqb.getName() + "在第 " + (j + 1) + " 天盈利成交, 买入价:"
+						+ buyPrice + " 卖出价 :" + winPrice + " 卖出当日涨幅:"
+						+ stockdailyinfoVO1.getZdf());
+				break;
+			}
+			if (lowPrice <= lostPrice) {
+				System.out.println(date + " " + sqb.getStockId() + " "
+						+ sqb.getName() + "在第 " + (j + 1) + " 天亏损成交, 买入价:"
+						+ buyPrice + " 卖出价 :" + lostPrice + " 卖出当日涨幅:"
+						+ stockdailyinfoVO1.getZdf());
+				break;
+			}
+		}
+	}
+
+}
--- src/main/java/com/ghlh/util/DateUtil.java
+++ src/main/java/com/ghlh/util/DateUtil.java
@@ -1,18 +1,40 @@
 package com.ghlh.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateUtil {
 	public static String formatDate(Date date) {
-		Date now = new Date();
 		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		return df.format(date);
+	}
+
+	public static String formatDay(Date date) {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		return df.format(date);
+	}
+
+	public static Date getNextDay(Date date) {
+		Date result = new Date(date.getTime() + 24 * 60 * 60 * 1000);
+		return result;
+	}
+
+	public static Date getDate(int year, int month, int day) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month);
+		calendar.set(Calendar.DAY_OF_MONTH, day);
+		calendar.set(Calendar.HOUR, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+		Date date = calendar.getTime();
+		return date;
 	}
 
 	public static void main(String[] args) {
 		Date now = new Date();
 		System.out.println(formatDate(now));
-		
+
 	}
 }

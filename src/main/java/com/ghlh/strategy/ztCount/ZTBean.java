3e37cd09e8006bad2a752f045b25fb3cafc45966
lqjliu <lqj.liu@gmail.com>
2014-04-21 07:43:38 +0800

    New  Feature

---
+++ src/main/java/com/ghlh/strategy/ztCount/ZTBean.java
@@ -0,0 +1,18 @@
+package com.ghlh.strategy.ztCount;
+
+public class ZTBean {
+	private String stockid;
+	public String getStockid() {
+		return stockid;
+	}
+	public void setStockid(String stockid) {
+		this.stockid = stockid;
+	}
+	public int getZtgs() {
+		return ztgs;
+	}
+	public void setZtgs(int ztgs) {
+		this.ztgs = ztgs;
+	}
+	private int ztgs;
+}
--- src/main/java/com/ghlh/strategy/ztCount/ZTCountAnalyst.java
+++ src/main/java/com/ghlh/strategy/ztCount/ZTCountAnalyst.java
@@ -10,21 +10,25 @@ import com.ghlh.util.KLineUtil;
 public class ZTCountAnalyst {
 
 	public static void main(String[] args) {
-		Date now = DateUtil.getDate(2014,2, 28);
+		Date now = new Date();
 
 		Date next = DateUtil.getNextMarketOpenDay(now);
-		Date weeksAgo = DateUtil.getPreviousMarketOpenDay(now, 10);
+		Date weeksAgo = DateUtil.getPreviousMarketOpenDay(now, 5);
 
-		String sql = "SELECT stockid FROM ( "
-				+ " SELECT stockid, COUNT(zdf) ztgs FROM stockdailyinfo WHERE zdf > 8  AND DATE > '"
+		String sql = "SELECT stockid, ztgs FROM ( "
+				+ " SELECT stockid, COUNT(zdf) ztgs FROM stockdailyinfo WHERE zdf > 4 AND highestprice != lowestprice AND todayopenprice != 0  AND DATE > '"
 				+ DateUtil.formatDay(weeksAgo)
 				+ "' and Date < '"
 				+ DateUtil.formatDay(next)
-				+ "' GROUP BY stockid) a WHERE a.ztgs > 1 ORDER BY a.ztgs DESC ";
-		List stocks = GhlhDAO.list(sql);
+				+ "' GROUP BY stockid) a WHERE a.ztgs >= 2 ORDER BY a.ztgs DESC ";
+		System.out.println("Sql = " + sql);
+
+		List stocks = GhlhDAO.list(sql, "com.ghlh.strategy.ztCount.ZTBean");
 		for (int j = 0; j < stocks.size(); j++) {
-			String stockId = (String) stocks.get(j);
-			KLineUtil.saveUrlAs(stockId, "ztgs\\" + DateUtil.formatDay(now));
+			ZTBean bean = (ZTBean) stocks.get(j);
+			KLineUtil.saveUrlAs(bean.getStockid(),
+					"ztgs\\" + DateUtil.formatDay(now), bean.getZtgs() + "_"
+							+ bean.getStockid());
 		}
 
 	}

19046c28c5389fc5610f147c70fb5f1dde25780f
lqjliu <lqj.liu@gmail.com>
2014-07-01 15:46:35 +0800

    multiple collecting

--- src/main/java/com/ghlh/autotrade/AutoTradeAfterCloseJob.java
+++ src/main/java/com/ghlh/autotrade/AutoTradeAfterCloseJob.java
@@ -1,4 +1,3 @@
-
 package com.ghlh.autotrade;
 
 import java.util.Date;
@@ -16,6 +15,7 @@ import com.ghlh.data.db.StocktradeVO;
 import com.ghlh.stockquotes.StockQuotesBean;
 import com.ghlh.strategy.TradeConstants;
 import com.ghlh.strategy.TradeUtil;
+import com.ghlh.util.DateUtil;
 import com.ghlh.util.EastMoneyUtil;
 import com.ghlh.util.StockMarketUtil;
 
@@ -37,7 +37,28 @@ public class AutoTradeAfterCloseJob implements Job {
 
 	private void collectStockDailyInfo() {
 		Date now = new Date();
-		new DataCollector().collectDailyInfo(now, false);
+		EventRecorder.recordEvent(this.getClass(), "开始收集收盘数据");
+		String today = DateUtil.formatDay(now);
+		for (int i = 0; i < 5; i++) {
+			new DataCollector().collectDailyInfo(now, false);
+			String sql = "SELECT COUNT(*) FROM stockdailyinfo WHERE "
+					+ "(stockid = '000001' OR stockid = '600036' OR stockid = '000002') AND DATE LIKE '"
+					+ today + "%'";
+			String value = GhlhDAO.selectSingleValue(sql);
+			int count = Integer.parseInt(value);
+			if (count != 0) {
+				EventRecorder.recordEvent(this.getClass(), "第 " + (i + 1)
+						+ "收集成功");
+				break;
+			} else {
+				if (i == 4) {
+					EventRecorder.recordEvent(this.getClass(), "第 " + (i + 1)
+							+ "收集 还是没有成功!");
+				}
+			}
+		}
+		EventRecorder.recordEvent(this.getClass(), "结束收集收盘数据");
+
 	}
 
 	public static void main(String[] args) {

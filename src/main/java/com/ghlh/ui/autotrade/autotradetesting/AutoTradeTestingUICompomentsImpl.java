1fe7fac0cef3b5baf347d6400671860901a0b962
Robin Liu <qijliu@cisco.com>
2016-01-28 21:14:21 -0800

    coding

--- src/main/java/com/ghlh/ui/autotrade/autotradetesting/AutoTradeTestingUICompomentsImpl.java
+++ src/main/java/com/ghlh/ui/autotrade/autotradetesting/AutoTradeTestingUICompomentsImpl.java
@@ -11,21 +11,21 @@ import com.ghlh.ui.bean.ComponentsBean;
 public class AutoTradeTestingUICompomentsImpl implements UIComponentsI {
 	public ComponentsBean getComponentsBean() {
 		List buttons = new ArrayList();
-		buttons.add("����");
-		buttons.add("�� ��");
+		buttons.add("买入");
+		buttons.add("卖 出");
 		ComponentsBean result = new ComponentsBean();
 		result.setButtons(buttons);
 		List components = new ArrayList();
 
 		UIComponentMetadata component1 = new UIComponentMetadata();
-		component1.setLabel("��Ʊ����");
+		component1.setLabel("股票代码");
 		component1.setCompomentType(UIComponentType.INT_FIELD);
 		UIComponentMetadata component2 = new UIComponentMetadata();
-		component2.setLabel("��Ʊ����");
+		component2.setLabel("股票名称");
 		component2.setCompomentType(UIComponentType.TEXT_FIELD);
 
 		UIComponentMetadata component3 = new UIComponentMetadata();
-		component3.setLabel("����");
+		component3.setLabel("数量");
 		component3.setCompomentType(UIComponentType.INT_FIELD);
 
 		components.add(component1);
